70cfc00e00bce88df8ef837ca11beb0a4015205e
Vahe <vahehovakimyan574@gmail.com>
2022-04-03 19:34:51 +0400

    ch

--- card_game.js
+++ card_game.js
@@ -1,4 +1,10 @@
-let card = document.getElementById("card");
+let random_num = Math.floor(Math.random() * 24);
+var card;// = document.getElementById("card" + random_num);
+// let variable_for_add = card + random_num
+
+for(let i = 0; i <= 24; i++){
+    card = document.getElementById("card" + i);
+}
 
 function Card_flip(){
     card.style.transform = "rotateY(180deg)";
