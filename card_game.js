2bf6a2d0b1a940805767eef674d744820594c71c
Vahe <vahehovakimyan574@gmail.com>
2022-04-03 19:55:35 +0400

    ch

--- card_game.js
+++ card_game.js
@@ -1,13 +1,25 @@
 let random_num = Math.floor(Math.random() * 24);
-var card;// = document.getElementById("card" + random_num);
+let card;
+let card_back = document.getElementById("card_back")
 // let variable_for_add = card + random_num
+var result;
+let cards;
+let current_card = document.getElementById("card" + random_num)
 
-for(let i = 0; i <= 24; i++){
-    card = document.getElementById("card" + i);
+card_back.style.backgroundColor = "red";
+
+for(let i = 1; i <= 24; i++){
+    cards = document.getElementById("card" + i);
+    // card.style.transform = "rotateY(180deg)";
+    // result = card;
 }
+console.log(result);
 
 function Card_flip(){
-    card.style.transform = "rotateY(180deg)";
+    current_card.style.transform = "rotateY(180deg)";
 }
 
-card.addEventListener("click",Card_flip);
+current_card.addEventListener("click",Card_flip);
+
+///////////////////////////////////////////////////////////////
+
