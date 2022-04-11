a58cbf9e4712cca5dd8db0a0d8100bf999b05e8e
Vahe <vahehovakimyan574@gmail.com>
2022-04-11 21:45:45 +0400

    ch

--- card_game.js
+++ card_game.js
@@ -2,22 +2,38 @@
 let random_num = Math.floor(Math.random() * 24);
 let bool = true;
 let a = [];
-
 let b = [];
 let Red = Math.floor(Math.random() * 255);
 let Blue = Math.floor(Math.random() * 255)
 let Green = Math.floor(Math.random() * 255)
 let BackCards = []
 
+
+let apple = "url(./images/apple.png)"
+let banana =  'url(./images/banana.png)'
+let blackberries =  'url(./images/blackberries.png)'
+let grapes =  'url(./images/grapes.png)'
+let kiwi =  'url(./images/kiwi.png)'
+let lemon =  'url(./images/lemon.png)'
+let lime =  'url(./images/lime.png)'
+let orange =  'url(./images/orange.png)'
+let pear =  'url(./images/pear.png)'
+let roseship =  'url(./images/roseship.png)'
+let strawberry =  'url(./images/strawberry.png)'
+let watermelon =  'url(./images/watermelon.png)'
+
+let fruits_arr = [apple,banana,blackberries,grapes,kiwi,lemon,lime,orange,pear,roseship,strawberry,watermelon];
+
+// console.log(fruits_arr);
+
+
 // ///////////////////////////////////////////////////////////////
 let FirstPartNumsArray = [];
 let SecondPartNumsArray = [];
 
 for (let i = 0; i <= 12; i++) {
-
     FirstPartNumsArray.push(i);
     SecondPartNumsArray.push(i)
-
 }
 
 function RandNumbers(array) {
@@ -27,17 +43,17 @@ function RandNumbers(array) {
 
     for (let i = array.length - 1; i > 0; i -= 1) {
 
-        j = Math.ceil(Math.random() * i)
+        j = Math.floor(Math.random() * (i + 1))
         temp = array[i]
         array[i] = array[j]
         array[j] = temp
     }
-
-    return array
+    return array;
 }
 
 let RandomNumsArr = [...RandNumbers(FirstPartNumsArray), ...RandNumbers(SecondPartNumsArray)]
 
+console.log(RandomNumsArr);
 
 let indexOfCards = [];
 let cards = [];
@@ -48,20 +64,24 @@ for (let i = 0; i <= 23; i++) {
 }
 
 
+//debugger
+let done_array = [];
+
+for (let i = 0; i <= 23; i++) {
+    BackCards[i].style.backgroundImage = fruits_arr[RandomNumsArr[i]];
+}
+//console.log(done_array);
 const RandomCOlorsofCards = () => {
 
     BackCards.forEach((item, index) => {
 
         indexOfCards.forEach(num => {
             if (index == num) {
-                item.style = `
-                    background-color:rgb(${Red},${Blue},${Green})
-                    `;
+                item.style.backgroundImage = fruits_arr[index]
+                item.style.backgroundSize = "cover"
             }
         })
-
     })
-
 }
 
 
