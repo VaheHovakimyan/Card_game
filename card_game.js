cc55aae4a17607ab29b469930f21811bc9e3579d
Vahe <vahehovakimyan574@gmail.com>
2022-04-11 18:14:32 +0400

    add images

--- card_game.js
+++ card_game.js
@@ -1,27 +1,87 @@
-let card = document.getElementById("card");
+//<<<<<<< HEAD
+let random_num = Math.floor(Math.random() * 24);
 let bool = true;
+let a = [];
 
-function Card_flip(){
-    card.style.transform = "rotateY(180deg)";
-    // bool = false;
+let b = [];
+let Red = Math.floor(Math.random() * 255);
+let Blue = Math.floor(Math.random() * 255)
+let Green = Math.floor(Math.random() * 255)
+let BackCards = []
+
+// ///////////////////////////////////////////////////////////////
+let FirstPartNumsArray = [];
+let SecondPartNumsArray = [];
+
+for (let i = 0; i <= 12; i++) {
+
+    FirstPartNumsArray.push(i);
+    SecondPartNumsArray.push(i)
+
 }
 
-function Card_opposite_flip(){
-    card.style.transform = "rotateY(0deg)";
-    //bool = true;
-}
+function RandNumbers(array) {
 
-function Flip_bool(){
-    if(bool){
-        Card_flip();
-        bool = false;
-    }else{
-        Card_opposite_flip();
-        bool = true;
+    let j = 0;
+    let temp = 0;
+
+    for (let i = array.length - 1; i > 0; i -= 1) {
+
+        j = Math.ceil(Math.random() * i)
+        temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
     }
+
+    return array
 }
 
-card.addEventListener("click", Flip_bool);
+let RandomNumsArr = [...RandNumbers(FirstPartNumsArray), ...RandNumbers(SecondPartNumsArray)]
+
+
+let indexOfCards = [];
+let cards = [];
+for (let i = 0; i <= 23; i++) {
+    indexOfCards.push(i)
+    cards.push(document.getElementById(`card${i}`));
+    BackCards.push(document.getElementById(`card_back${i}`))
+}
+
+
+const RandomCOlorsofCards = () => {
+
+    BackCards.forEach((item, index) => {
+
+        indexOfCards.forEach(num => {
+            if (index == num) {
+                item.style = `
+                    background-color:rgb(${Red},${Blue},${Green})
+                    `;
+            }
+        })
+
+    })
+
+}
+
+
+
+const Cards_flip = () => {
+    cards.forEach((it, index) => {
+        it.addEventListener("click", () => {
+            indexOfCards.forEach(num => {
+                if (index == num) {
+                    it.style = `
+                    transform:rotateY(180deg);
+                    `;
+                }
+            })
+        });
+    }
+    )
+}
+
+Cards_flip();
+RandomCOlorsofCards();
 
 ///////////////////////////////////////////////////////////////
-
