a6d9c31471062ee579d1e585e8d2cf775737bb54
Vahe4836 <88284245+Vahe4836@users.noreply.github.com>
2022-09-04 20:28:55 +0400

    ch

--- card_game.js
+++ card_game.js
@@ -1,18 +1,18 @@
 //<<<<<<< HEAD
 
 let BackCards = [];
-let apple = 'url(./images/apple.png)'
-let banana = 'url(./images/banana.png)'
-let blackberries = 'url(./images/blackberries.png)'
-let grapes = 'url(./images/grapes.png)'
-let kiwi = 'url(./images/kiwi.png)'
-let lemon = 'url(./images/lemon.png)'
-let lime = 'url(./images/lime.png)'
-let orange = 'url(./images/orange.png)'
-let pear = 'url(./images/pear.png)'
-let rosehip = 'url(./images/rosehip.png)'
-let strawberry = 'url(./images/strawberry.png)'
-let watermelon = 'url(./images/watermelon.png)'
+let apple = 'url(./images/apple.png)';
+let banana = 'url(./images/banana.png)';
+let blackberries = 'url(./images/blackberries.png)';
+let grapes = 'url(./images/grapes.png)';
+let kiwi = 'url(./images/kiwi.png)';
+let lemon = 'url(./images/lemon.png)';
+let lime = 'url(./images/lime.png)';
+let orange = 'url(./images/orange.png)';
+let pear = 'url(./images/pear.png)';
+let rosehip = 'url(./images/rosehip.png)';
+let strawberry = 'url(./images/strawberry.png)';
+let watermelon = 'url(./images/watermelon.png)';
 
 let fruits_arr = [apple, banana, blackberries, grapes, kiwi, lemon, lime, orange, pear, rosehip, strawberry, watermelon];
 
@@ -21,14 +21,16 @@ let score = 0;
 let game_end = document.getElementById("end_game");
 let reload_button = document.getElementById("game_reload");
 
-function Reload_game(){
+
+function Reload_game() {
     document.location.reload();
 }
-reload_button.addEventListener("click",Reload_game);
+reload_button.addEventListener("click", Reload_game);
 
 // ///////////////////////////////////////////////////////////////
 let FirstPartNumsArray = [];
 let SecondPartNumsArray = [];
+
 
 for (let i = 0; i < 12; i++) {
     FirstPartNumsArray.push(i);
@@ -71,7 +73,7 @@ const RandomCOlorsofCards = () => {
 
         indexOfCards.find(num => {
 
-            if (index == num) {
+            if (index === num) {
                 item.style = `
                 background-image :${fruits_arr[RandomNumsArr[index]]};
                 background-size:cover;
@@ -86,80 +88,112 @@ const RandomCOlorsofCards = () => {
 
 let timer;
 
+// function Main_logic() {
+//     if (+current[0].current_value === +current[1].current_value) {
+//         current[0].style = `
+//         visibility: hidden;
+//         opacity: 0;
+//         transition: 0.5s ease;
+//         transform: rotateY(90deg);
+//         pointer-events: none;
+//         `;
 
-const Cards_flip = () => {
+//         current[1].style = `
+//         visibility: hidden;
+//         opacity: 0;
+//         transition: 0.5s ease;
+//         transform: rotateY(90deg);
+//         pointer-events: none;
+//         `;
+//         score++;
+
+//         if (score == 12) {
+//             end_game.style.top = "30%";
+//         }
+//         current = [];
+//     } else {
+//         current[0].style = `
+//         transition: 0.5s ease;
+//         transform:rotateY(0deg);
+
+//         `;
+//         current[1].style = `
+//         transition: 0.5s ease;
+//         transform:rotateY(0deg);
+//         `;
+//         current = [];
+//     }
+// }
+
+
+const Cards_flip = (callback) => {
 
     let current = [];
 
 
-    cards.find((it, index) => {
+    cards.map((it, index) => {
 
         it.current_value = RandomNumsArr[index];
         it.addEventListener("click", function () {
             indexOfCards.forEach(num => {
-                if (index == num) {
+                if (index === num) {
                     it.style = `
                     // transition: 0.5s ease;
                     transform:rotateY(180deg);
                     pointer-events: none;
                     `
-                    if(current.length < 2){
+                    if (current.length < 2) {
                         current.push(it);
                     }
+
                     console.log(current);
-                    //console.log(current.length);
-                     if (current.length == 2) {
-                        //  cards.forEach((itt) => {
-                        //     itt.style = `
-                        //     pointer-events: none;
-                        //     `
-                        //  })
 
-                        /* function Check(){*/setTimeout(() => {
-                            if (+current[0].current_value === +current[1].current_value) {
-                                current[0].style = `
-                                visibility: hidden;
-                                opacity: 0;
-                                transition: 0.5s ease;
-                                transform: rotateY(90deg);
-                                pointer-events: none;
-                                `;
+                    function Main_logic() {
+                        if (+current[0].current_value === +current[1].current_value) {
+                            current[0].style = `
+                            visibility: hidden;
+                            opacity: 0;
+                            transition: 0.5s ease;
+                            transform: rotateY(90deg);
+                            pointer-events: none;
+                            `;
+                    
+                            current[1].style = `
+                            visibility: hidden;
+                            opacity: 0;
+                            transition: 0.5s ease;
+                            transform: rotateY(90deg);
+                            pointer-events: none;
+                            `;
 
-                                current[1].style = `
-                                visibility: hidden;
-                                opacity: 0;
-                                transition: 0.5s ease;
-                                transform: rotateY(90deg);
-                                pointer-events: none;
-                                `;
-                                score++;
-                                
-                                if(score == 12){
-                                    end_game.style.top = "30%";
-                                }
-                                current = [];
-                                //clearTimeout(timer);
-                            } else {
-                                current[0].style = `
-                                transition: 0.5s ease;
-                                transform:rotateY(0deg);
-
-                                `;
-                                current[1].style = `
-                                transition: 0.5s ease;
-                                transform:rotateY(0deg);
-                                `;
-                                current = [];
-                                //clearTimeout(timer);
+                            score++;
+                    
+                            if (score == 12) {
+                                end_game.style.top = "30%";
                             }
 
-                        // cards.forEach((itt) => {
-                        //     itt.style = `
-                        //     pointer-events: auto;
-                        //     `
-                        // })
-                        },500)
+                            current = [];
+                        } else {
+                            current[0].style = `
+                            transition: 0.5s ease;
+                            transform:rotateY(0deg);
+                            pointer-events: none;
+                            `;
+                            current[1].style = `
+                            transition: 0.5s ease;
+                            transform:rotateY(0deg);
+                            pointer-events: none;
+                            `;
+                            current = [];
+                        }
+                    }
 
+                    if (current.length === 2) {
+                        var func = setTimeout(() => {
+                            callback(Main_logic(current))
+                        }, 1000);
+                    }else{
+                        clearTimeout(func)
                     }
 
                 }
@@ -169,5 +203,8 @@ const Cards_flip = () => {
 }
 
 
-Cards_flip();
+Cards_flip(function (current) {
+    alert(current.length);
+});
+
 RandomCOlorsofCards();
